61fa4a84d01c28e9d7526d9520ba2947193e2907
1729372667@qq.com <1729372667@qq.com>
2024-07-06 16:26:25 +0800

    update scripts

--- eval_hybridrescore_VO.sh
+++ eval_hybridrescore_VO.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 #SBATCH --account=yfliu3
-#SBATCH --job-name=rescore_AO
+#SBATCH --job-name=rescore_VO
 #SBATCH --partition=debug # 用sinfo命令可以看到所有队列
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1 # 若多卡或多进程，请调整此参数
 #SBATCH --cpus-per-task=16  # 每个进程的CPU数量
 #SBATCH --gres=gpu:1        # 若使用2块卡，则gres=gpu:2
-#SBATCH --output=eval_hybridrescore_AO.out
-#SBATCH --error=eval_hybridrescore_AO.err
+#SBATCH --output=eval_hybridrescore_VO.out
+#SBATCH --error=eval_hybridrescore_VO.err
 
 python eval.py \
 --eval_lrs3_model_file /data2/alumni/gryang/Leveraging-Self-Supervised-Learning-for-AVSR-main/check/train-step_0604-wer_0.054.ckpt \
---modal AO \
+--modal VO \
 --decode_type HYBRID_RESCORE \
---logname /data1/yfliu/Leveraging-Self-Supervised-Learning-for-AVSR-main_noneed/beam40_AO/ao_0.8.txt \
+--logname /data1/yfliu/Leveraging-Self-Supervised-Learning-for-AVSR-main_noneed/beam40_VO/ao_0.8.txt \
 --beamWidth 40 \
 --batch_size 48 \
 --beta 0.8 \
